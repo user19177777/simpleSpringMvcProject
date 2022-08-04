ba9bba396c238807ff4af26fb84206ee1ca1a42d
Ivan Ivanov <79523701529@yandex.ru>
2022-08-04 16:08:34 +0300

    add method to add person @ModelAttribute

--- src/main/java/springMvcApp/controllers/PeopleController.java
+++ src/main/java/springMvcApp/controllers/PeopleController.java
@@ -3,10 +3,9 @@ package springMvcApp.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import springMvcApp.dao.PersonDAO;
+import springMvcApp.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -32,5 +31,23 @@ public class PeopleController {
         //получим одного человека по id из ДАО и передадим в представление
         model.addAttribute("person",personDAO.show(id));
         return "people/show";
+    }
+    @GetMapping("/new")
+    public String newPerson(
+            //создает новый объект Person по пустому конструктору и добавляет его в модель
+            @ModelAttribute("person")Person person){
+        return "people/new";
+    }
+    @PostMapping
+    public String create(
+            //создает новый объект Person (данные полей берет из URL) и добавляет его в модель
+            @ModelAttribute("person")Person person){
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+    //помещает данные в модели всех методов контроллера
+    @ModelAttribute("message")
+    public String message(){
+        return "Don't forget!";
     }
 }
