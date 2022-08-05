93803df145d44449e92a21abdd3804a8de7f19d2
Ivan Ivanov <79523701529@yandex.ru>
2022-08-05 17:22:07 +0300

    add CRUD methods

--- src/main/java/springMvcApp/controllers/MessagesController.java
+++ src/main/java/springMvcApp/controllers/MessagesController.java
@@ -1,11 +1,60 @@
 package springMvcApp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import springMvcApp.dao.MessageDAO;
+import springMvcApp.models.Message;
 
 @Controller
 @RequestMapping("/messages")
 public class MessagesController {
+    private final MessageDAO messageDAO;
+    @Autowired
+    public MessagesController(MessageDAO messageDAO) {
+        this.messageDAO = messageDAO;
+    }
 
-
+    //READ
+    @GetMapping
+    public String index(Model model){
+        model.addAttribute("messages",messageDAO.index());
+        return "messages/index";
+    }
+    //READ
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id")int id,Model model){
+        model.addAttribute("message",messageDAO.show(id));
+        return "messages/show";
+    }
+    // show new message form
+    @GetMapping("/new")
+    public String newMessage(@ModelAttribute("message")Message message){
+        return "messages/new";
+    }
+    //send POST from /new
+    @PostMapping
+    public String create(@ModelAttribute("message")Message message){
+        messageDAO.save(message);
+        return "redirect:/messages";
+    }
+    //show edit message form
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable("id")int id,Model model){
+        model.addAttribute("message",messageDAO.show(id));
+        return "messages/edit";
+    }
+    //send UPDATE(PATCH)from /id/edit
+    @PatchMapping("/{id}")
+    public String update(@PathVariable("id")int id,@ModelAttribute("message")Message message){
+        messageDAO.update(id,message);
+        return "redirect:/messages";
+    }
+    //DELETE
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id")int id){
+        messageDAO.delete(id);
+        return "redirect:/messages";
+    }
 }
--- src/main/java/springMvcApp/dao/MessageDAO.java
+++ src/main/java/springMvcApp/dao/MessageDAO.java
@@ -1,7 +1,37 @@
 package springMvcApp.dao;
 
 import org.springframework.stereotype.Component;
+import springMvcApp.models.Message;
+
+import java.util.*;
 
 @Component
 public class MessageDAO {
+    private List<Message>messages;
+    private static int MESSAGE_COUNT;
+    {
+        messages = new ArrayList<>();
+        messages.add(new Message(++MESSAGE_COUNT,"hoho"));
+        messages.add(new Message(++MESSAGE_COUNT,"fefe"));
+        messages.add(new Message(++MESSAGE_COUNT,"bebe"));
+    }
+
+    public List<Message>index(){
+        return messages;
+    }
+
+    public Message show(final int id){
+        return messages.stream().filter(message->message.getId()==id).findAny().orElse(null);
+    }
+    public void save(Message message){
+        message.setId(++MESSAGE_COUNT);
+        messages.add(message);
+    }
+    public void update(int id,Message message){
+        Message message1=show(id);
+        message1.setText(message.getText());
+    }
+    public void delete(int id){
+        messages.removeIf(m->m.getId()==id);
+    }
 }
