722bfab84e8554e859553f7f9af04555d3203dcb
Ivan Ivanov <79523701529@yandex.ru>
2022-08-05 15:58:01 +0300

    add controller,model,DAO

---
+++ src/main/java/springMvcApp/controllers/MessagesController.java
@@ -0,0 +1,11 @@
+package springMvcApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/messages")
+public class MessagesController {
+
+
+}
---
+++ src/main/java/springMvcApp/dao/MessageDAO.java
@@ -0,0 +1,7 @@
+package springMvcApp.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageDAO {
+}
