f1bda0dd4d23327a7d78e9c2e15c4de0a26fb176
Ivan Ivanov <79523701529@yandex.ru>
2022-08-12 17:37:04 +0300

    add JDBC driver support

--- src/main/java/springMvcApp/dao/PersonDAO.java
+++ src/main/java/springMvcApp/dao/PersonDAO.java
@@ -3,41 +3,82 @@ package springMvcApp.dao;
 import org.springframework.stereotype.Component;
 import springMvcApp.models.Person;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
-    private static int PEOPLE_COUNT;
-    private List<Person> people;
-    {
-        people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT,"Tom",20,"sdf@fdg.ru"));
-        people.add(new Person(++PEOPLE_COUNT,"Den",25,"sdf@fdg.ru"));
-        people.add(new Person(++PEOPLE_COUNT,"Bob",28,"sdf@fdg.ru"));
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static Connection connection;
+
+
+    static {
+        try {
+            // проверяет с помощью подгрузился ли драйвер JDBC в оперативную память
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            //создает  подключение к базе данных,тк static - один раз
+            connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
+
     //READ CRUD
-    public List<Person> index(){
+    public List<Person> index()  {
+        List<Person>people = new ArrayList<>();
+        try {
+            //создает объект для sql запроса
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM person";
+            //помещает результат запроса в resultSet,запрос не меняет данные в базе
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while (resultSet.next()){
+                Person person = new Person();
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setAge(resultSet.getInt("age"));
+                person.setEmail(resultSet.getString("email"));
+
+                people.add(person);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return people;
     }
     //READ CRUD
     public Person show (int id){
-        return people.stream().filter(person->person.getId()==id).findAny().orElse(null);
+
+        return null;
     }
     //CREATE CRUD
     public void save(Person person){
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "INSERT INTO Person VALUES(" + 1 + ",'" + person.getName() + "'," + person.getAge() + ",'" + person.getEmail() + "')";
+            //меняет данные в базе(создает запись в базе)
+            statement.executeUpdate(SQL);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
     //UPDATE CRUD
     public void update(int id,Person updatedPerson){
-        Person personToBeUpdated = show(id);
-        personToBeUpdated.setName(updatedPerson.getName());
-        personToBeUpdated.setAge(updatedPerson.getAge());
-        personToBeUpdated.setEmail(updatedPerson.getEmail());
+//        Person personToBeUpdated = show(id);
+//        personToBeUpdated.setName(updatedPerson.getName());
+//        personToBeUpdated.setAge(updatedPerson.getAge());
+//        personToBeUpdated.setEmail(updatedPerson.getEmail());
     }
     //DELETE CRUD
-    public void delete(int id){
-        people.removeIf(p->p.getId()==id);
+    public void delete(int id){;
     }
 }
