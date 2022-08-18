abffe78921ead4022f14a3bd692a6ff0f32cadf2
Ivan Ivanov <79523701529@yandex.ru>
2022-08-18 13:29:27 +0300

    id SERIAL

--- src/main/java/springMvcApp/models/Person.java
+++ src/main/java/springMvcApp/models/Person.java
@@ -1,20 +1,19 @@
 package springMvcApp.models;
 
-
-//import javax.validation.constraints.Email;
-//import javax.validation.constraints.Min;
-//import javax.validation.constraints.NotEmpty;
-//import javax.validation.constraints.Size;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 public class Person {
     private int id;
-    //@NotEmpty(message = "Should be not empty")
-    //@Size(min = 2,max = 15,message = "Should be between 2 and 15  characters")
+    @NotEmpty(message = "Should be not empty")
+    @Size(min = 2,max = 15,message = "Should be between 2 and 15  characters")
     private String name;
-    //@Min(value = 0,message = "Should be greater than 0")
+    @Min(value = 0,message = "Should be greater than 0")
     private int age;
-    //@NotEmpty(message = "Should be not empty")
-    //@Email(message = "Should be valid")
+    @NotEmpty(message = "Should be not empty")
+    @Email(message = "Should be valid")
     private String email;
 
     public Person() {
@@ -47,9 +46,7 @@ public class Person {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public void setId(int id) {this.id = id;}
 
     public String getName() {
         return name;
